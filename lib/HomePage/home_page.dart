be6ea9c22b71ef792d396ea0824caf2edfbed7f2
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-01-12 23:36:45 +0100

    Added home page screen

--- lib/HomePage/home_page.dart
+++ lib/HomePage/home_page.dart
@@ -1,24 +1,170 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedOne = 0;
+  @override
   Widget build(BuildContext context) {
+    final List<String> textCategory = [
+      'All Stores',
+      'OutDoor',
+      'Training',
+      'Tenis',
+    ];
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(
-          top: 8.0,
-          left: 20.0,
-          right: 20.0,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 8.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Column(
+            children: [
+              Image.asset('assets/images/Group 1000000742.png'),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 20.0,
+                  bottom: 10.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 60.0,
+                      width: 250,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.white,
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Looking For Shoes',
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15.0,
+                            horizontal: 15.0,
+                          ),
+                          prefixIcon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //
+              //
+              //
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Select Category',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: textCategory.length,
+                      itemBuilder: (context, index) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedOne = index;
+                          });
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                            right: 20.0,
+                            top: 10.0,
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10.0,
+                            horizontal: 15.0,
+                          ),
+                          height: 40,
+                          // width: 70.0,
+                          decoration: BoxDecoration(
+                            color: selectedOne == index
+                                ? Colors.blue
+                                : Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(),
+                          ),
+                          child: Center(
+                            child: Text(
+                              textCategory[index],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              //
+              //
+              //
+              //
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          'Popular Shoes',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        Text(
+                          'See all',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      height: 170.0,
+                      width: 170.0,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Image.asset('assets/icons/Vector (19).png'),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            Image.asset('assets/images/Group 1000000742.png'),
-          ],
-        ),
-      )),
+      ),
     );
   }
 }
